219868f7e83c0035f375567dcf5e44b6aec34b6b
kishoor <kishoor0920@gmail.com>
2024-06-22 15:32:48 +0530

    update settings

--- leads_form/settings.py
+++ leads_form/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k1ft4+cd&w^zf8c6irxhbmv46c%sx44^+_lp9snx$ho_i^h@&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['leads.yavar.ai', 'www.leads.yavar.ai']
-CSRF_TRUSTED_ORIGINS = ['https://leads.yavar.ai']
+CSRF_TRUSTED_ORIGINS = ['https://leads.yavar.ai','leads.yavar.ai', 'www.leads.yavar.ai']
 
 
 # Secure cookies in production
